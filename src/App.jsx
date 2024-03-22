831881ae9c1726a306cf5bfabcc8f5a499e3a3a8
unknown <acaoy2@gmail.com>
2024-03-22 16:06:40 -0400

    some requirements finished

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,64 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [current, setCurrent] = useState({})
+  const [history, setHistory] = useState([])
+
+  const headers = new Headers({
+    "Content-Type": "application/json",
+    "x-api-key": "live_hbN3XjSGMN99JWMOlNvQZII9Z6FtMorF0fv0V9ICuzYatmnmAVZI8pbxYw8pM5PX"
+  });
+
+  var requestOptions = {
+    method: "GET",
+    headers: headers,
+    redirect: 'follow'
+  };
+
+  async function getPicture() {
+    console.log(history)
+    await fetch("https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      if (current.url != undefined) {
+        setHistory([...history, current])
+      }
+      setCurrent(result[0])
+    })
+    .catch(error => console.log('error', error));
+  }
+
+  function renderAnimal() {
+    if (current.url !== undefined) {
+      return (
+        <img className="dog-image" src={current.url}></img>
+      )
+    }
+  }
+
+  function renderHistory() {
+    return history.map((animal, key) => {
+      return (
+        <img className="history-image" src={animal.url} key={key}></img>
+      )
+    })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+      <div className='history'>
+        <h1>History</h1>
+        {renderHistory()}
+        </div>
+        <div className='middle-section'>
+          {renderAnimal()}
+          <button onClick={getPicture}>Call API</button>
+        </div>
+        <div className='ban-list'>
+          <h1>Banned List</h1>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
